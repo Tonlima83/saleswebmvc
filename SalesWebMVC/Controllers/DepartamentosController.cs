b05d0906dc05e30b64aae5a3d798bfb2006d4437
Tonlima83 <tonlima.lima@gmail.com>
2023-09-14 17:12:54 -0300

    Primeiro modelo ControleView Departamento

---
+++ SalesWebMVC/Controllers/DepartamentosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Controllers {
+    public class DepartamentosController : Controller {
+        public IActionResult Index() {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento { Id = 1, Nome = "Eletrônicos" });
+            list.Add(new Departamento { Id = 2, Nome = "Fashion" });
+            return View(list);
+        }
+    }
+}
---
+++ SalesWebMVC/Models/Departamento.cs
@@ -0,0 +1,7 @@
+﻿namespace SalesWebMVC.Models {
+    public class Departamento {
+        public int Id { get; set; }
+        public string Nome { get; set;}
+
+    }
+}
